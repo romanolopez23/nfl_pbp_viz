ee63726dbb15443bc9d648093716368a29d4e2e7
Romain Lopez <63277550+romanolopez23@users.noreply.github.com>
2024-10-14 22:36:49 -0400

    Update nfl_viz.py

--- nfl_viz.py
+++ nfl_viz.py
@@ -233,21 +233,18 @@ if play_type_choice == 'run':
         f"{away_team} at {home_team} on {game_date}\n"
         f"Rusher: {currentplayer}\n"
         f"Down: {down} - Yards to Go: {ydstogo}\n\n"
+        f"{wrapped_desc}"
     )
 elif play_type_choice == 'pass':
     currenttitle = (
         f"{away_team} at {home_team} on {game_date}\n"
         f"{currentplayer} to {receiver}\n"
         f"Down: {down} - Yards to Go: {ydstogo}\n\n"
+        f"{wrapped_desc}"
     )
 
 # Set the title for the plot
 plt.title(currenttitle, fontsize=24)
-
-st.markdown(
-    f"""<p style="font-size:12px;">{wrapped_desc}</p>""", 
-    unsafe_allow_html=True
-)
 
 # Display the figure in Streamlit
 st.pyplot(fig, use_container_width=True)
